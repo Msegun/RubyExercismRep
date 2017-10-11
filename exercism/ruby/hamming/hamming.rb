9a542e9a0279a7e49fbec22b89f4c0c244419118
msegun <msegun@localhost.localdomain>
2017-10-11 17:26:34 +0200

    hammind done 15/16 Test OK

---
+++ exercism/ruby/hamming/hamming.rb
@@ -0,0 +1,16 @@
+# top level class comment
+class Hamming
+  def self.compute(dna1, dna2)
+    raise ArgumentError if dna1.length != dna2.length
+
+    difference = 0
+    i = 0
+
+    while i < dna1.length
+      difference += 1 if dna1[i] != dna2[i]
+      i += 1
+    end
+
+    difference
+  end
+end
